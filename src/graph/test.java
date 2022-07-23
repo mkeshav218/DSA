9d7c3480055a4b8740f113e188886bd64de4b9b7
mkeshav218 <mishrakeshav218@gmail.com>
2022-07-23 16:29:20 +0530

    test updated

--- src/graph/test.java
+++ src/graph/test.java
@@ -1,29 +1,45 @@
 package graph;
 
+import java.util.Scanner;
+
 public class test {
 
 	public static void main(String[] args) {
-		int n  = 5;
-		addEdge(n);
-		System.out.println(n);
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		System.out.println(encrypt(s));
+		System.out.println(decrypt(s));
 	}
 	
-	static void addEdge(int n) {
-		n = 10;
-	}
-	
-	static void updateElem(int[] a) {
-		a[3] = 100;
-		printAdjList(a);
-	}
-	
-	static void printAdjList(int[] a) {
-		System.out.println("****** Array Elements ******");
-		for(int i:a) {
-			System.out.print(i+" ");
+	private static String encrypt(String s) {
+		StringBuffer sb = new StringBuffer();
+		boolean increment = true;
+		for(int i=0;i<s.length();i++) {
+			char ch = s.charAt(i);
+			if(increment) {
+				ch = (char) ((int)ch + 1);
+			}else {
+				ch = (char) ((int)ch - 1);
+			}
+			sb.append(ch);
+			increment = !increment;
 		}
-		System.out.println("\n");
+		return sb.toString();
 	}
-	
+	private static String decrypt(String s) {
+		StringBuffer sb = new StringBuffer();
+		boolean increment = false;
+		for(int i=0;i<s.length();i++) {
+			char ch = s.charAt(i);
+			if(increment) {
+				ch = (char) ((int)ch + 1);
+			}else {
+				ch = (char) ((int)ch - 1);
+			}
+			sb.append(ch);
+			increment = !increment;
+		}
+		return sb.toString();
+	}
 
 }
