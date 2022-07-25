be1874b4b163b9ed489f0bf013811f72357cb4a7
mkeshav218 <mishrakeshav218@gmail.com>
2022-07-25 16:36:28 +0530

    Shortest path updated

--- src/graph/ShortestPath_in_UndirectedGraph.java
+++ src/graph/ShortestPath_in_UndirectedGraph.java
@@ -1,7 +1,8 @@
 package graph;
 /***
  * 
- * Shortest Path between two nodes
+ * Given source & destination node 
+ * Find Shortest Path between two given nodes
  * v :- No of Nodes
  * e :- No of Edges
  * Time Complexity :- O(v+e)  
@@ -30,9 +31,73 @@ public class ShortestPath_in_UndirectedGraph {
 		boolean[] visited = new boolean[9];
 		LinkedList<Integer> shortestPath = new LinkedList<>();
 		LinkedList<Integer> currentPath = new LinkedList<>();
-		System.out.println("Length of Shortest path = " + findShortestPath(adjList, 1, 8, visited,shortestPath,currentPath));
-		System.out.println("Shortest Path = " + shortestPath);
+		System.out.println("Length of Shortest path = " + findShortestPathDFS(adjList, 2, 7, -1,visited,shortestPath,currentPath));
+		System.out.println("Shortest Path using DFS = " + shortestPath);
+		
+		System.out.println("Shortest Path using BFS = " + findShortestPathBFS(adjList, 2, 7,8));
 	}
+
+	static int findShortestPathDFS(HashMap<Integer,List<Integer>> adjList,int src,int dest,int parent,boolean[] visited,LinkedList<Integer> shortestPath,LinkedList<Integer> currentPath) {
+		LinkedList<Integer> allNodes = (LinkedList<Integer>) adjList.get(src);
+		visited[src] = true;
+		currentPath.add(src);
+		if(allNodes.contains(dest)) {
+			currentPath.add(dest);
+			System.out.println(currentPath);
+			if(shortestPath.size()==0 || shortestPath.size()>currentPath.size()) {
+				shortestPath.clear();
+				for(int i:currentPath) {
+					shortestPath.add(i);
+				}
+				currentPath.removeLast();
+				currentPath.removeLast();
+			}
+			return 1;
+		}else {
+			int min = Integer.MAX_VALUE;
+			for(int i:allNodes) {
+				if(i!=parent) {
+					int ans =  findShortestPathDFS(adjList, i, dest,src, visited,shortestPath,currentPath);
+					if(ans<min) {
+						min = ans;
+					}
+				}
+			}
+			currentPath.removeLast();
+			return min + 1;
+		}
+	}
+	
+	static List<Integer> findShortestPathBFS(HashMap<Integer,List<Integer>> adjList,int src,int dest,int v){
+		int[] nodeToParentMap = new int[v+1];
+		LinkedList<Integer> ans = new LinkedList<>();
+		LinkedList<Integer> q = new LinkedList<>();
+		boolean[] visited = new boolean[100];
+		q.addLast(src);
+		nodeToParentMap[src] = -1;
+		while(!q.isEmpty()) {
+			int node = q.removeFirst();
+			visited[node] = true;
+			LinkedList<Integer> allConnectedNodes = (LinkedList<Integer>) adjList.get(node);
+			for(int i:allConnectedNodes) {
+				if(!visited[i]) {
+					q.addLast(i);
+					if(nodeToParentMap[i]==0) {
+						nodeToParentMap[i]=node;
+					}
+				}
+			}
+		}
+		int parent = -1;
+		ans.addFirst(dest);
+		while(parent!=src) {
+			parent = nodeToParentMap[dest];
+			ans.addFirst(parent);
+			dest = parent;
+		}
+		return ans;
+	}
+	
 
 	//Add new edge
 	static void addEdge(HashMap<Integer,List<Integer>> adjList,int u,int v) {
@@ -67,35 +132,5 @@ public class ShortestPath_in_UndirectedGraph {
 		System.out.println("\n");
 	}
 
-	static int findShortestPath(HashMap<Integer,List<Integer>> adjList,int src,int dest,boolean[] visited,LinkedList<Integer> shortestPath,LinkedList<Integer> currentPath) {
-		LinkedList<Integer> allNodes = (LinkedList<Integer>) adjList.get(src);
-		visited[src] = true;
-		currentPath.add(src);
-		if(allNodes.contains(dest)) {
-			currentPath.add(dest);
-			if(shortestPath.size()==0 || shortestPath.size()>currentPath.size()) {
-				shortestPath.clear();
-				for(int i:currentPath) {
-					shortestPath.add(i);
-				}
-				currentPath.removeLast();
-				currentPath.removeLast();
-			}
-			return 1;
-		}else {
-			int min = Integer.MAX_VALUE;
-			for(int i:allNodes) {
-				if(!visited[i]) {
-					int ans =  findShortestPath(adjList, i, dest, visited,shortestPath,currentPath);
-					if(ans<min) {
-						min = ans;
-					}
-				}
-			}
-			currentPath.removeLast();
-			return min + 1;
-		}
-
-	}
 
 }
