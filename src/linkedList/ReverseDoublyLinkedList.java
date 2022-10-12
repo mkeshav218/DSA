654a8b20782adf206b9aec254fdb4b0dc0c77919
Keshav Mishra <mishrakeshav218@gmail.com>
2022-10-12 08:11:28 +0530

    Reverse of DLL implemented

--- src/linkedList/ReverseDoublyLinkedList.java
+++ src/linkedList/ReverseDoublyLinkedList.java
@@ -16,33 +16,40 @@ public class ReverseDoublyLinkedList {
 		list.insertAtEnd(9);
 		list.insertAtEnd(10);
 		list.insertAtEnd(11);
+		list.insertAtEnd(12);
 
 
 		list.printList();
 		System.out.println("Head = " + list.head.data + ", Tail = " + list.tail.data + ", Length = " + list.length);
 
-//		System.out.println("\nReversing DLL Iteratively :- ");
-//		list.tail = list.head;
-//		list.head = reverse(list.head);
-//		list.printList();
-//		System.out.println("Head = " + list.head.data + ", Tail = " + list.tail.data + ", Length = " + list.length);
-//
-//		System.out.println("\nReversing DLL By swapping next & prev pointer :- ");
-//		list.tail = list.head;
-//		list.head = reverseDLList(list.head);
-//		list.printList();
-//		System.out.println("Head = " + list.head.data + ", Tail = " + list.tail.data + ", Length = " + list.length);
-//
-//		System.out.println("\nReversing DLL By Appending each element of the list to a new Empty List :- ");
-//		list.tail = list.head;
-//		list.head = reverseDLL(list.head);
-//		list.printList();
-//		System.out.println("Head = " + list.head.data + ", Tail = " + list.tail.data + ", Length = " + list.length);
+		System.out.println("\nReversing DLL Iteratively :- ");
+		list.tail = list.head;
+		list.head = reverse(list.head);
+		list.printList();
+		System.out.println("Head = " + list.head.data + ", Tail = " + list.tail.data + ", Length = " + list.length);
+
+		System.out.println("\nReversing DLL By swapping next & prev pointer :- ");
+		list.tail = list.head;
+		list.head = reverseDLList(list.head);
+		list.printList();
+		System.out.println("Head = " + list.head.data + ", Tail = " + list.tail.data + ", Length = " + list.length);
+
+		System.out.println("\nReversing DLL By Appending each element of the list to a new Empty List :- ");
+		list.tail = list.head;
+		list.head = reverseDLL(list.head);
+		list.printList();
+		System.out.println("Head = " + list.head.data + ", Tail = " + list.tail.data + ", Length = " + list.length);
 
 		System.out.println("**********      Reversing in K group     ************");
-		int k = 4;
-		list.head = reverseInKgroup(list.head, k);
+		int k = 12;
+		System.out.println("K = " + k);
+		list.head = reverseInKgroupIteratively(list.head, k);
 		list.printList();
+
+		System.out.println("\nK = " + k);
+		list.head = reverseInKgroupRecursively(list.head, k);
+		list.printList();
+
 	}
 	
 	/***
@@ -124,7 +131,16 @@ public class ReverseDoublyLinkedList {
 		return temp;
 	}
 	
-	public static NodeDLL reverseInKgroup(NodeDLL head,int k) {
+	/***
+	 * 
+	 * Time Complexity :- O(n)
+	 * Space Complexity :- O(n/k) = O(n)
+	 * 
+	 * Each recursive call returns the reversed-Head 
+	 * And, If the size of last group is less than K then reverse it and return it
+	 * 
+	 */
+	public static NodeDLL reverseInKgroupRecursively(NodeDLL head,int k) {
 		if(head==null || head.next==null)
 			return head;
 		NodeDLL prevPtr = null,curr=head,nextPtr=head.next;
@@ -140,13 +156,65 @@ public class ReverseDoublyLinkedList {
 			counter++;
 		}
 		if (counter >= k) {
-            NodeDLL tempHead = reverseInKgroup(curr, k);
+            NodeDLL tempHead = reverseInKgroupRecursively(curr, k);
             head.next = tempHead;
             if (tempHead != null)
             	tempHead.prev = head;
+		}else {
+			prevPtr = reverse(prevPtr);
 		}
 
 		return prevPtr;
 	}
 
+	/***
+	 * 
+	 * Time Complexity :- O(n)
+	 * Space Complexity :- O(1)
+	 * 
+	 * Reverse the list in a group of K size 
+	 * And, if size of last group is less than K then maintain its previous state
+	 * 
+	 */
+	public static NodeDLL reverseInKgroupIteratively(NodeDLL head,int k) {
+		if(head==null || head.next==null)
+			return head;
+		NodeDLL prevPtr = null,curr=head,nextPtr=head.next;
+		NodeDLL newHead = null,prevHead=null;
+		int counter = 0;
+		while(curr!=null) {
+			if(counter<k) {
+				curr.next = prevPtr;
+				curr.prev = nextPtr;
+				prevPtr = curr;
+				curr = nextPtr;
+				if(nextPtr!=null) {
+					nextPtr = nextPtr.next;
+				}
+				counter++;
+			}else {
+				if(newHead==null) {
+					newHead = prevPtr;
+					prevHead = head;
+					head = curr;
+				}else {
+					prevHead.next = prevPtr;
+					prevPtr.prev = prevHead;
+					prevHead = head;
+					head = curr;
+				}
+				prevPtr = null;
+				counter = 0;
+			}
+		}
+		if(counter<k) {
+			prevHead.next = reverse(prevPtr);
+		}else {
+			if(prevHead!=null)
+				prevHead.next = prevPtr;
+			else
+				newHead = prevPtr;
+		}
+		return newHead;
+	}
 }
