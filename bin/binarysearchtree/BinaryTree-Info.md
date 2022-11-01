2c74eca506d963ec93a8293d571201e3a0a69540
Keshav Mishra <mishrakeshav218@gmail.com>
2022-11-01 17:47:41 +0530

    BST added

---
+++ bin/binarysearchtree/BinaryTree-Info.md
@@ -0,0 +1,2 @@
+# BinarySearchTree-Info 
+[BinaryTree-Basic-Ops](https://github.com/mkeshav218/DSA/blob/master/src/binarytree/BinaryTree.java) \
---
+++ src/binarysearchtree/TreeNode.java
@@ -0,0 +1,16 @@
+package binarysearchtree;
+
+public class TreeNode {
+	int data;
+	TreeNode left;
+	TreeNode right;
+	
+	public TreeNode(int data) {
+		super();
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+	
+	
+}
