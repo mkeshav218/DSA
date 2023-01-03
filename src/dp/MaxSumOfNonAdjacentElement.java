6ae81fa292a93545aec287a8334aa464a7c74119
Keshav Mishra <keshavkumarmishra>
2023-01-03 14:07:55 +0530

    Max Sum of Non Adjacent Element added

---
+++ src/dp/MaxSumOfNonAdjacentElement.java
@@ -0,0 +1,77 @@
+package dp;
+
+/***
+ * 
+ * In an array of positive numbers, find the maximum sum of a subsequence,
+ * such that no 2 numbers in the sequence should be adjacent in the array.
+ *
+ */
+public class MaxSumOfNonAdjacentElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {3, 2, 7, 10};//{5, 5, 10, 100, 10, 5};//
+		
+		System.out.println("Using Tabulation :- " + getMaxSumTab(arr));
+		System.out.println("Using Memoization :- " + getMaxSumMem(arr));
+		System.out.println("Using Space Optimization :- " + getMaxSumSO(arr));
+
+	}
+	
+	/***
+	 * Time Complexity :- O(n)
+	 * Space Complexity :- O(1)
+	 */
+	public static int getMaxSumSO(int[] arr) {
+		int n = arr.length;
+		int includeSum = arr[0];
+		int excludeSum = 0;
+		for(int i=1;i<n;i++) {
+			int currentIncludeSum = arr[i] + excludeSum;
+			int currentExcludeSum = Math.max(includeSum, excludeSum);
+			includeSum = currentIncludeSum;
+			excludeSum = currentExcludeSum;
+		}
+		return Math.max(includeSum, excludeSum);
+	}
+	
+	/***
+	 * Time Complexity :- O(n)
+	 * Space Complexity :- O(n)
+	 */
+	public static int getMaxSumTab(int[] arr) {
+		int n = arr.length;
+		int[] includeSum = new int[n];
+		int[] excludeSum = new int[n];
+		includeSum[0] = arr[0];
+		for(int i=1;i<n;i++) {
+			includeSum[i] = arr[i] + excludeSum[i-1];
+			excludeSum[i] = Math.max(includeSum[i-1], excludeSum[i-1]);
+		}
+		return Math.max(includeSum[n-1], excludeSum[n-1]);
+	}
+	
+	/***
+	 * Time Complexity :- O(n)
+	 * Space Complexity :- O(n)
+	 */
+	public static int[] include,exclude;
+	public static int getMaxSumMem(int[] arr) {
+		int n = arr.length;
+		include = new int[n];
+		exclude = new int[n];
+		getMaxSumMemUtil(arr, n-1);
+		return Math.max(include[n-1], exclude[n-1]);
+	}
+	public static void getMaxSumMemUtil(int[] arr,int n) {
+		if(n==0) {
+			include[0] = arr[0];
+			exclude[0] = 0;
+			return;
+		}
+		getMaxSumMemUtil(arr, n-1);
+		include[n] = arr[n] + exclude[n-1];
+		exclude[n] = Math.max(include[n-1],exclude[n-1]);
+	}
+
+}
--- src/dp/dp-Info.md
+++ src/dp/dp-Info.md
@@ -1,3 +1,4 @@
 # Dp-Info 
 [Fibonacci](https://github.com/mkeshav218/DSA/blob/master/src/dp/Fibonacci.java) \
 [FrogJump](https://github.com/mkeshav218/DSA/blob/master/src/dp/FrogJump.java) \
+[MaxSumOfNonAdjacentElement](https://github.com/mkeshav218/DSA/blob/master/src/dp/MaxSumOfNonAdjacentElement.java) \
